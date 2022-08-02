be2e0d1885138c012c1f8c9bc9cbfa16b8a8a2bf
Tuditi <daviddetroch@pm.me>
2022-08-02 13:34:08 +0200

    feat: added tailwind + postcss

---
+++ postcss.config.cjs
@@ -0,0 +1,19 @@
+const path = require('path')
+const mode = process.env.NODE_ENV || 'development'
+
+module.exports = {
+    plugins: [
+        require('tailwindcss')('./tailwind.config.cjs'),
+        require('postcss-url')({
+            url:
+                mode === 'production'
+                    ? (asset) =>
+                          path
+                              .relative(__dirname, asset.absolutePath)
+                              .replace('..', '../shared')
+                              // on Windows the path resolution doesn't work unless we change the path separator
+                              .replace(/\\/g, '/')
+                    : undefined,
+        }),
+    ],
+}
